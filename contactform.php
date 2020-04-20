83070e39f4633f37e0a8524b6c60808963826363
Joel Robinson <44164513+JR325243@users.noreply.github.com>
2020-04-20 01:00:03 +0100

    retry

---
+++ contactform.php
@@ -0,0 +1,16 @@
+<?php
+
+if(isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo = "robojoe97@hotmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "Message from ".$name.":\n\n".$message;
+
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
