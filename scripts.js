397209cadcb9e4e7b2e821ce768d2b30d33e07b9
Joel Robinson <44164513+JR325243@users.noreply.github.com>
2020-12-01 11:04:32 +0000

    changes

---
+++ scripts.js
@@ -0,0 +1,14 @@
+var currentPage = "home";
+
+function show(content) {
+    document.getElementById(currentPage).style.display = 'none';
+    document.getElementById(content).style.display = "block";
+
+    currentPage = content;
+    topFunction();
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
