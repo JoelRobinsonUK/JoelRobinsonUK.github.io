a7b30656dcb3c6470bf76b723151119914fa1c68
JoelRobinsonUK <49420265+JoelRobinsonUK@users.noreply.github.com>
2021-04-19 15:06:41 +0100

    Started a major Rework

--- scripts.js
+++ scripts.js
@@ -12,3 +12,7 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+function close(){
+    window.close();
+}
