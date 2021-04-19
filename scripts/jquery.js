d3b7a744288edf1bf206435ae3c257a2c35e7de0
JoelRobinsonUK <49420265+JoelRobinsonUK@users.noreply.github.com>
2021-04-19 19:48:56 +0100

    fuck you

---
+++ scripts/jquery.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.modal').modal();
+    $('.sidenav').sidenav();
+    $('.collapsible').collapsible();
+    $('.carousel').carousel({
+        indicators: true,
+        fullWidth: true
+    });
+});
+$('.tooltipped').tooltip();
+$('.scrollspy').scrollSpy();
